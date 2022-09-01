9ee930079ccaaeec8b4922bb577cf4cd6ae5ceae
Danial-Waheed <daniyalwahied@gmail.com>
2022-09-01 14:10:31 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <h1>Magic Match</h1>
+        <button>New Game</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
